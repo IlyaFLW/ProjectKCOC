8f3227674d35f854165facd6912eee1844e6b572
IlyaFLW <65287583+IlyaFLW@users.noreply.github.com>
2023-12-01 13:53:24 +0300

    Added victory screen

--- JustTryna/Assets/Scripts/lvl1popupfail.cs
+++ JustTryna/Assets/Scripts/lvl1popupfail.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class lvl1popupfail : MonoBehaviour
 {
     public TMPro.TMP_Text ScoreText;
+    public GameObject Vic;
     
     public GameObject ext;
+    public GameObject Vext;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +27,13 @@ public class lvl1popupfail : MonoBehaviour
             Time.timeScale = 0;
             ext.SetActive(true);
             
+        }
+        if (ScoreText.text == "5")
+        {
+            Time.timeScale = 0;
+            Vic.SetActive(true);
+            Vext.SetActive(true);
+
         }
     }
     
